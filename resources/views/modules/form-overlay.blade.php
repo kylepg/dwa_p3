2305967cc37d2fd97b91bfab412d5c533800787e
kylepg <kylepgeorge@gmail.com>
2018-04-06 10:30:11 -0400

    fixed 'x' image path

--- resources/views/modules/form-overlay.blade.php
+++ resources/views/modules/form-overlay.blade.php
@@ -1,7 +1,7 @@
 <div class="form-overlay {{ count($errors)> 0 || session('success') ? 'active' : '' }}">
     <div class="container">
         <div class="notification-form py-4 px-5 col-sm-6 col-xs-12">
-            <img class="close-button" src="{{ asset('storage/images/x.png') }} " alt="Close button" />
+            <img class="close-button" src="{{ URL::to('/') }}/storage/images/x.png" alt="Close button" />
             <form class="{{ session( 'success') ? '' : 'active' }}" method='POST' action='{{ Request::path() }}'> {{ csrf_field() }}
                 <fieldset>
                     <label class="mt-3 " for='firstname'>* First name</label>
